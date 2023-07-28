8c1bfb23aac205c7dc248a003cd195987779d44a
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-07-28 23:19:04 +0530

    Time: 0 ms (100%), Space: 11.1 MB (88.59%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    //COMPLEXITY REDUCTION
+    //FURTHER COMPLEXITY REDUCTION
     int maxProfit(vector<int>& prices) {
         int profit=0;
         int n=prices.size();
-        vector<vector<int>> dp(n+2,vector<int> (2,0));
+        vector<int> front2(2,0),front1(2,0),curr(2,0);
         for(int index=n-1;index>=0;index--){
-            dp[index][1]=max(-prices[index]+dp[index+1][0],0+dp[index+1][1]);
-            dp[index][0]=max(prices[index]+dp[index+2][1],0+dp[index+1][0]);
+            curr[1]=max(-prices[index]+front1[0],0+front1[1]);
+            curr[0]=max(prices[index]+front2[1],0+front1[0]);
+            front2=front1;
+            front1=curr;
         }
-        return dp[0][1];    //as hum n-1 se 0 par jaa rahe hei toh answer will be stored in 0 only 
+        return curr[1];    //as hum n-1 se 0 par jaa rahe hei toh answer will be stored in 0 only 
     }
+
+
+    //COMPLEXITY REDUCTION
+    // int maxProfit(vector<int>& prices) {
+    //     int profit=0;
+    //     int n=prices.size();
+    //     vector<vector<int>> dp(n+2,vector<int> (2,0));
+    //     for(int index=n-1;index>=0;index--){
+    //         dp[index][1]=max(-prices[index]+dp[index+1][0],0+dp[index+1][1]);
+    //         dp[index][0]=max(prices[index]+dp[index+2][1],0+dp[index+1][0]);
+    //     }
+    //     return dp[0][1];    //as hum n-1 se 0 par jaa rahe hei toh answer will be stored in 0 only 
+    // }
 
 
     //TABULATION
