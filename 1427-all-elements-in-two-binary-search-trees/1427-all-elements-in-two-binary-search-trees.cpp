a10df58add8f99e2c61d0bab8ac10be5b945d59b
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-07-29 22:27:34 +0530

    Time: 113 ms (99.54%), Space: 86.7 MB (25.31%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.cpp
@@ -11,18 +11,36 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int> & ans){
-        if(root==NULL) return;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
+    void inorder(TreeNode* root, vector<int> &v){
+        if(!root) return;
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
     }
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2){
-        if(root1==NULL && root2==NULL) return {};
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> v1, v2;
+        inorder(root1, v1);
+        inorder(root2, v2);
         vector<int> ans;
-        inorder(root1,ans);
-        inorder(root2,ans);
-        sort(ans.begin(),ans.end());
+        merge(v1.begin(),v1.end(),v2.begin(),v2.end(),back_inserter(ans)); 
+		// Merge function takes log(m+n) as compared with sort function which takes nlogn. 
         return ans;
     }
+
+
+    //USING SORT FUNCTION
+    // void inorder(TreeNode* root,vector<int> & ans){
+    //     if(root==NULL) return;
+    //     inorder(root->left,ans);
+    //     ans.push_back(root->val);
+    //     inorder(root->right,ans);
+    // }
+    // vector<int> getAllElements(TreeNode* root1, TreeNode* root2){
+    //     if(root1==NULL && root2==NULL) return {};
+    //     vector<int> ans;
+    //     inorder(root1,ans);
+    //     inorder(root2,ans);
+    //     sort(ans.begin(),ans.end());
+    //     return ans;
+    // }
 };
