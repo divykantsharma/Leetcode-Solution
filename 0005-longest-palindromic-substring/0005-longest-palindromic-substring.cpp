12dd1970bf65cb3f956a845818ce2c5190962988
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-07-31 08:02:06 +0530

    Time: 8 ms (96.82%), Space: 7 MB (68.8%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -5,70 +5,67 @@ output="acaa"
 expected output="aca"
 We are following the approach striver told in longest palindromic subsequence - which wont work here - as it is returning LCSubstring and not LPSubstring 
 Difference is jo standard approach se string aayegi voh aage peeche se toh same hoegi par individually khud ek palindrome ni hei*/
+//Therefore changes done :
+// string longestPalindrome(string s){
+//     int n=s.length();
+//     string t=s;  
+//     reverse(t.begin(),t.end());
+//     vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+//     int maxi=0,start=0;
+//     //no need of base casse as 0 hi daal rahe hei , but we initialized dp with 0
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             if(s[i-1]==t[j-1]){
+//                 dp[i][j]=1+dp[i-1][j-1];
+//                 int rIdx=i;
+//                 int idx=j-dp[i][j]+1;
+//                 if(n-rIdx+1==idx){
+//                     if(maxi<dp[i][j]){
+//                         maxi=dp[i][j];
+//                         start=i;
+//                     }
+//                 }
+//             }
+//             else dp[i][j]=0;
+//         }
+//     }
+//     string ans;
+//     while(maxi>0){
+//         ans.push_back(s[start-1]);
+//         start--;maxi--;
+//     }
+//     return ans;
+// }
+
+
+//Odd Even Method
 string longestPalindrome(string s){
-    int n=s.length();
-    string t=s;  
-    reverse(t.begin(),t.end());
-    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-    int maxi=0,start=0;
-    //no need of base casse as 0 hi daal rahe hei , but we initialized dp with 0
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            if(s[i-1]==t[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];
-                int rIdx=i;
-                int idx=j-dp[i][j]+1;
-                if(n-rIdx+1==idx){
-                    if(maxi<dp[i][j]){
-                        maxi=dp[i][j];
-                        start=i;
-                    }
-                }
+        int n=s.length();
+        int pos=0,len=1;
+        int high,low;
+        for(int i=1;i<n;i++){
+            low=i;      // for even
+            high=i-1;
+            while(low>=0 && high<n && s[low]==s[high]){
+               if(len<high-low+1){
+                   pos=low;
+                   len=high-low+1;
+               }
+               low--,high++;
             }
-            else dp[i][j]=0;
+            low=i-1;    // for odd
+            high=i+1;
+            while(low>=0 && high<n && s[low]==s[high]){
+                if(len<high-low+1){
+                    pos=low;
+                    len=high-low+1;
+                    
+                }
+                low--,high++;
+            }
         }
+        return s.substr(pos,len);
     }
-    string ans;
-    while(maxi>0){
-        ans.push_back(s[start-1]);
-        start--;maxi--;
-    }
-    return ans;
-}
-
-
-//MEMOIZATION- shifting of indexes
-// int f(int i,int j,string& s,string& t,vector<vector<int>>& dp){
-//     if(i==0 || j==0) return 0;
-//     if(dp[i][j]!=-1) return dp[i][j];
-//     if(s[i-1]==t[j-1]) return dp[i][j]=1+f(i-1,j-1,s,t,dp);
-//     else return dp[i][j]=f(i-1,j-1,s,t,dp);
-// }
-// string longestPalindrome(string s){
-//     int n=s.length();
-//     string t=s;   //t string is reverse of s therefore the LCSubstring of s and t will be a PALINDROME only 
-//     reverse(t.begin(),t.end());
-//     vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-//     cout<<f(n,n,s,t,dp);        //printing the length of longest palindromic subsequence , need a DP TABLE
-//     return s;
-// }
-
-
-//MEMOIZATION
-// int f(int i,int j,string& s,string& t,vector<vector<int>>& dp){
-//     if(i<0 || j<0) return 0;
-//     if(dp[i][j]!=-1) return dp[i][j];
-//     if(s[i]==t[j]) return dp[i][j]=1+f(i-1,j-1,s,t,dp);
-//     else return dp[i][j]=f(i-1,j-1,s,t,dp);
-// }
-// string longestPalindrome(string s){
-//     int n=s.length();
-//     string t=s;   //t string is reverse of s therefore the LCSubstring of s and t will be a PALINDROME only 
-//     reverse(t.begin(),t.end());
-//     vector<vector<int>> dp(n,vector<int>(n,-1));
-//     cout<<f(n-1,n-1,s,t,dp);   //printing the length of LPSubsequence
-//     return s;
-// }
 
 
 //LOOPS - TC:O(N^2)*O(N) - MLE - (49/141) testcases passed
