9b49674c126fe3233ce4f1866012e39b569c226c
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-01 09:33:03 +0530

    Time: 113 ms (95.81%), Space: 54.9 MB (51%) - LeetHub

--- 1330-longest-arithmetic-subsequence-of-given-difference/1330-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1330-longest-arithmetic-subsequence-of-given-difference/1330-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
+    //14 July - Daily Streak - NOW DONE
+    //TC->O(N) , SC->O(N)
     int longestSubsequence(vector<int>& arr, int difference){
-        int ans=0;
-        //14 July - though samajh aagya apar firse karo
+        int n=arr.size();
+        int maxi=1;
         unordered_map<int,int> dp;
-        for(int i=0;i<arr.size();i++){
+        for(int i=0;i<n;i++){
             int temp=arr[i]-difference;
             int tempans=0;
-            if(dp.find(temp)!=dp.end()){
-                tempans=dp[temp];
-            }
+            if(dp.find(temp)!=dp.end()) tempans=dp[temp];
             dp[arr[i]]=1+tempans;
-
-            ans=max(ans,dp[arr[i]]);
+            maxi=max(maxi,dp[arr[i]]);
         }
-        return ans;
+        return maxi;
     }
+
+
+    //TLE - TC->O(N^2) - STRIVER APPROACH
+    // int longestSubsequence(vector<int>& arr, int difference){
+    //     int n=arr.size();
+    //     int maxi=1;
+    //     vector<int> dp(n,1);
+    //     for(int i=0;i<n;i++){
+    //         for(int prev=0;prev<i;prev++){
+    //             if((arr[i]-arr[prev])==difference && 1+dp[prev]>dp[i]){
+    //                 dp[i]=1+dp[prev];
+    //             }
+    //         }
+    //         maxi=max(maxi,dp[i]);
+    //     }
+    //     return maxi;
+    // }
 };
