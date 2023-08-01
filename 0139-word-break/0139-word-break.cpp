d59a3c9092b64a82351a08cbcec8620d01e5aa82
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-01 11:46:58 +0530

    Time: 23 ms (34.04%), Space: 13.6 MB (32.7%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    //RECURSION - TLE - (35/46) testcases passed
+    //MEMOIZATION
     bool f(int pos,string& s,unordered_set<string>& st,vector<int>& dp){
         if(pos==s.size()) return true;
         if(dp[pos]!=-1) return dp[pos];
-        string t="";
         for(int i=pos;i<s.size();i++){
             if(st.find(s.substr(pos,i-pos+1))!=st.end() && f(i+1,s,st,dp)){
                 return dp[pos]=true;
@@ -18,4 +17,21 @@ public:
         vector<int> dp(n,-1);
         return f(0,s,st,dp);
     }
+
+
+    //RECURSION - TLE - (35/46) testcases passed
+    // bool f(int pos,string& s,unordered_set<string>& st){
+    //     if(pos==s.size()) return true;
+    //     string t="";
+    //     for(int i=pos;i<s.size();i++){
+    //         if(st.find(s.substr(pos,i-pos+1))!=st.end() && f(i+1,s,st)){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    // bool wordBreak(string s,vector<string>& wordDict){
+    //     unordered_set<string> st(wordDict.begin(), wordDict.end());
+    //     return f(0,s,st,dp);
+    // }
 };
