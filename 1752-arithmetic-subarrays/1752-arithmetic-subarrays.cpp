650d095b644057fa1bea831bd0690433df20c1f8
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-02 17:38:10 +0530

    Time: 41 ms (96.4%), Space: 25.7 MB (79.6%) - LeetHub

---
+++ 1752-arithmetic-subarrays/1752-arithmetic-subarrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isCheck(vector<int>& temp){
+        sort(temp.begin(),temp.end());
+        // if(temp.size()<=2) return true;
+        int diff=temp[1]-temp[0];
+        for(int i=2;i<temp.size();i++){
+            if(temp[i]-temp[i-1]!=diff) return false;
+        }
+        return true;
+    }
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums,vector<int>& l,vector<int>& r){
+        int n=nums.size(),m=l.size();
+        vector<bool> ans;
+        for(int i=0;i<m;i++){
+            vector<int> temp(nums.begin()+l[i],nums.begin()+r[i]+1);
+            // int st=l[i],en=r[i];
+            // vector<int> temp;
+            // for(int j=st;j<=en;j++){
+            //     temp.push_back(nums[st]);
+            // }
+            ans.push_back(isCheck(temp));
+        }
+        return ans;
+    }
+};
