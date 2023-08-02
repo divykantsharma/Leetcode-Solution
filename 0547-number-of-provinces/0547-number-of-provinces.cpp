790a2d0e55e479890eff848e39d929be3aa2b925
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-02 15:13:27 +0530

    Time: 30 ms (48.53%), Space: 16.9 MB (10.02%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void dfs(int node,vector<int> adjLs[],vector<int>& vis){
+        vis[node]=1;
+        for(auto it:adjLs[node]){
+            if(!vis[it]){
+                dfs(it,adjLs,vis);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected){
+        int V=isConnected.size();
+        vector<int> adjLs[V];   //CREATING OUR OWN ADJACENCY LIST
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    adjLs[i].push_back(j);
+                    adjLs[j].push_back(i);
+                }
+            }
+        }
+        vector<int> vis(V,0);
+        int count=0;    //count will contain the number of provinces we have
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                count++;        //jitni baar dfs call alag se karna padega utni hi province hongi
+                dfs(i,adjLs,vis);   
+            }
+        }
+        return count;
+    }
+};
