0f5a2ad398c7ff20303144afb647a57a4bbbc203
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-04 14:49:11 +0530

    Time: 17 ms (93.94%), Space: 13.7 MB (67.67%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    //13 July - Daily Streak - NOW DONE
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites){
+        int N=numCourses;
+	    vector<int> adj[N];
+        for(auto it:prerequisites){
+            adj[it[0]].push_back(it[1]);
+        }
+	    // for(int i=0;i<prerequisites.size();i++){
+	    //     adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+	    // }
+        vector<int> indegree(N,0);
+        for(int i=0;i<N;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<N;i++){
+            if(indegree[i]==0){
+               q.push(i); 
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        return ans.size()==N;
+    }
+};
