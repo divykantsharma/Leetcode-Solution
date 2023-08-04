16964cd1a607e389583ee7d9d44afc897adde4cd
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-04 12:46:27 +0530

    Time: 38 ms (90.12%), Space: 18.4 MB (87.11%) - LeetHub

--- 1572-subrectangle-queries/1572-subrectangle-queries.cpp
+++ 1572-subrectangle-queries/1572-subrectangle-queries.cpp
@@ -1,20 +1,20 @@
 class SubrectangleQueries{
 public:
-    vector<vector<int>> rec; 
+    vector<vector<int>> rectangle; 
     SubrectangleQueries(vector<vector<int>>& rectangle){
-        rec=rectangle;
+        this->rectangle=rectangle;
     }
     
     void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue){
         for(int i=row1;i<=row2;i++){
             for(int j=col1;j<=col2;j++){
-                rec[i][j]=newValue;
+                this->rectangle[i][j]=newValue;
             }
         }
     }
     
     int getValue(int row, int col){
-        return rec[row][col];
+        return this->rectangle[row][col];
     }
 };
 
