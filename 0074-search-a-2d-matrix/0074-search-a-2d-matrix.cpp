2ee7a3a03d5dec37aa4677e4105f39e73f7396af
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-07 07:33:14 +0530

    Time: 0 ms (100%), Space: 9.5 MB (52.82%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    //BINARY SEARCH - TC->O(log(M*N)) - as the given matrix is SORTED
+    bool searchMatrix(vector<vector<int>>& matrix,int target){
+        int n=matrix.size(),m=matrix[0].size();
+        int i=0,j=m-1;
+        while(i>=0 && i<n && j>=0 && j<m){
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]>target) j--;
+            else if(matrix[i][j]<target) i++;
+        }
+        return false;
+    }
+};
