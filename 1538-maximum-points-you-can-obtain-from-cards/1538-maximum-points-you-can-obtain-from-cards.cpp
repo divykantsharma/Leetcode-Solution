cc37b9af02790fe4aa7bf7ea02befb8695cbf6ac
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-09 19:48:52 +0530

    Time: 47 ms (91.37%), Space: 42.4 MB (55.21%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,10 +1,11 @@
 class Solution{
 public:
+    //SLIDING WINDOW
     int maxScore(vector<int>& cardPoints,int k){
         int totalSum=accumulate(cardPoints.begin(),cardPoints.end(),0);
         int n=cardPoints.size();
-        int winSize=n-k;
         if(n==k) return totalSum;
+        int winSize=n-k;
         int maxi=0,sum=0;
         int i=0,j=0;
         while(j<n){
@@ -18,4 +19,47 @@ public:
         }
         return maxi;
     }
+
+
+    //MEMOIZATION , MLE - (31,40) testcases passed
+    // int f(int i,int j,vector<int>& cardPoints,int k,vector<vector<int>>& dp){
+    //     if(k==0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int start=cardPoints[i]+f(i+1,j,cardPoints,k-1,dp);
+    //     int end=cardPoints[j]+f(i,j-1,cardPoints,k-1,dp);
+    //     return dp[i][j]=max(start,end);
+    // }
+    // int maxScore(vector<int>& cardPoints,int k){
+    //     int n=cardPoints.size();
+    //     vector<vector<int>> dp(n,vector<int>(n,-1));
+    //     return f(0,n-1,cardPoints,k,dp);
+    // }
+
+
+    //MEMOIZATION , MLE - (26,40) testcases passed
+    // int f(int i,int j,vector<int>& cardPoints,int k,vector<vector<vector<int>>>& dp){
+    //     if(k==0) return 0;
+    //     if(dp[i][j][k]!=-1) return dp[i][j][k];
+    //     int start=cardPoints[i]+f(i+1,j,cardPoints,k-1,dp);
+    //     int end=cardPoints[j]+f(i,j-1,cardPoints,k-1,dp);
+    //     return dp[i][j][k]=max(start,end);
+    // }
+    // int maxScore(vector<int>& cardPoints,int k){
+    //     int n=cardPoints.size();
+    //     vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int>(k+1,-1)));
+    //     return f(0,n-1,cardPoints,k,dp);
+    // }
+
+
+    //RECUSION , TLE - (22/40) testcases passed
+    // int f(int i,int j,vector<int>& cardPoints,int k){
+    //     if(k==0) return 0;
+    //     int start=cardPoints[i]+f(i+1,j,cardPoints,k-1);
+    //     int end=cardPoints[j]+f(i,j-1,cardPoints,k-1);
+    //     return max(start,end);
+    // }
+    // int maxScore(vector<int>& cardPoints,int k){
+    //     int n=cardPoints.size();
+    //     return f(0,n-1,cardPoints,k);
+    // }
 };
