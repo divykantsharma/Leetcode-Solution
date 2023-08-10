ee70981cfa448e515722cf079cd1ac3dfa8cb675
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-10 11:31:54 +0530

    Time: 1381 ms (7.91%), Space: 51.5 MB (5.93%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,39 @@
+//https://www.youtube.com/watch?v=EgMPjWliYGY
+class Solution {
+public:
+    //MEMOIZATION
+    bool f(int i,vector<int>& nums,vector<int>& dp){
+        int n=nums.size();
+        if(i==n-1) return  true;
+        if(nums[i]==0) return false;
+        if(dp[i]!=-1) return dp[i];
+        for(int jump=i+1;jump<=i+nums[i];jump++){
+            if(jump<nums.size() && f(jump,nums,dp)){
+                return dp[i]=true;
+            }
+        }
+        return dp[i]=false;
+    }
+    bool canJump(vector<int>& nums){
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return f(0,nums,dp);
+    }
+
+
+    //RECURSION , TLE - (75/172) testcases passed
+    // bool f(int i,vector<int>& nums){
+    //     int n=nums.size();
+    //     if(i==n-1) return  true;
+    //     if(nums[i]==0) return false;
+    //     for(int jump=i+1;jump<=i+nums[i];jump++){
+    //         if(jump<nums.size() && f(jump,nums)){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    // bool canJump(vector<int>& nums){
+    //     return f(0,nums);
+    // }
+};
