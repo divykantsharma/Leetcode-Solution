0651bd3bd417cbfb538864813efbc9a5c70d3f24
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-11 17:56:33 +0530

    Time: 2 ms (44.69%), Space: 6 MB (40.43%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    //if number of set bits in a number equals 1 , then the number is a power of 2
-    // bool isPowerOfTwo(int n){	
-    //     int count=0;
-    //     for(int i=0;i<32;i++){
-    //         if(n&(1<<i)) count++;
-    //     }
-    //     return count==1;
-    // }
-
-
     bool isPowerOfTwo(int n){
         for(int i=0;i<32;i++){
             if(n==pow(2,i)){
@@ -18,5 +8,16 @@ public:
         }
         return false;
     }
+
+
+    //if number of set bits in a number equals 1 ,then the number is a power of 2
+    //Misses on the last test case (1108/1109) testcases passed
+    // bool isPowerOfTwo(int n){	
+    //     int count=0;
+    //     for(int i=0;i<32;i++){
+    //         if(n&(1<<i)) count++;
+    //     }
+    //     return count==1;
+    // }
 };
 
