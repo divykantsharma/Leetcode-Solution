cc6743adfb44d85f02ec8225eda12c710f2daba8
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-26 12:26:00 +0530

    Time: 35 ms (19.49%), Space: 45.6 MB (10.32%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,51 @@
+//MUST LEARN a more optimized solution striver bhaiya is telling in his video
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string> board,int n){
+        int duprow=row;
+        int dupcol=col;
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q') return false;
+            row--;
+            col--;
+        }
+        col=dupcol;
+        row=duprow;
+        while(col>=0){
+            if(board[row][col]=='Q') return false;
+            col--;
+        }
+        row=duprow;
+        col=dupcol;
+        while(row< n && col>=0){
+            if(board[row][col]=='Q') return false;
+            row++;
+            col--;
+        }
+        return true;
+    }
+    void solve(int col,vector<string>& board,vector<vector<string>>& ans,int n){
+        //Base Case
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(row,col,board,n)){     //checking is it safe to place the queen at this pos
+                board[row][col]='Q';
+                solve(col+1,board,ans,n);
+                board[row][col]='.';            //Back tracking
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n){
+        vector<vector<string>> ans;
+        vector<string> board(n);        //board - [[],[],[],[]]
+        string s(n,'.');                //s - "...."
+        for(int i=0;i<n;i++){
+            board[i]=s;                 //board - [[ "...."],[ "...."],[ "...."],[ "...."]]
+        }
+        solve(0,board,ans,n);
+        return ans;
+    }
+};
