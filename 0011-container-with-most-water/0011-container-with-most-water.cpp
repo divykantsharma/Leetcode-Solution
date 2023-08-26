734b3bd98d1f2e3c0710eae8511b24528a07ec63
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-26 14:50:34 +0530

    Time: 66 ms (85.07%), Space: 59.1 MB (27.59%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        //OPTIMIZED APPROACH - TC : O(N)
-        int n=height.size();
-        int left=0;
-        int right=n-1;
+    int maxArea(vector<int>& height){
+        int st=0,en=height.size()-1;
         int area=0;
-        while(left<right){
-            int length=right-left;
-            int breadth=min(height[left],height[right]);
-            area=max(area,length*breadth);
-            if(height[left]>height[right]) right--;
-            else left++;
+        while(st<en){
+            area=max(area,min(height[st],height[en])*(en-st));
+            (height[st]<height[en])?st++:en--;
         }
         return area;
 
-        
-        //NAIVE APPROACH - TC : O(N^2)
+
+        //OPTIMIZED APPROACH - TC : O(N)
+        // int n=height.size();
+        // int left=0;
+        // int right=n-1;
+        // int area=0;
+        // while(left<right){
+        //     int length=right-left;
+        //     int breadth=min(height[left],height[right]);
+        //     area=max(area,length*breadth);
+        //     if(height[left]>height[right]) right--;
+        //     else left++;
+        // }
+        // return area;
+
+
+        // NAIVE APPROACH - TC : O(N^2) - TLE
         // int maximum=0;
         // for(int i=0;i,height.size();i++){
         //     for(int j=i+1;j<height.size();j++){
