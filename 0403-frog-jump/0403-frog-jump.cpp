36a4f7d4b08ccdf38945118702ae6a597cd624fa
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-27 08:37:41 +0530

    Time: 40 ms (97.41%), Space: 232.2 MB (22.28%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -21,22 +21,21 @@ public:
     }
 
 
-    //RECURSION - TLE (15/52) TC passed
+    //RECURSION - TLE (16/52) TC passed
     // bool f(int index,int jump,vector<int>& stones){
     //     if(index==stones.size()-1) return true;
-    //     bool ans=false;
     //     for(int i=index+1;i<stones.size();i++){
     //         if(stones[i]-stones[index]>jump+1) break;
     //         for(int j=-1;j<2;j++){
     //             if(stones[i]-stones[index]==jump+j){
-    //                 ans=f(i,jump+j,stones)||ans;
+    //                 if(f(i,jump+j,stones)) return true;;
     //             }
     //         }
     //     }
-    //     return ans;
+    //     return false;
     // }
     // bool canCross(vector<int>& stones){
-    //     if(stones[1]-stones[0]>1) return false;
-    //     return f(0,1,stones);
+    //     int n=stones.size();
+    //     return f(0,0,stones);
     // }
 };
