e8d30cd4361d436f2a3c93bb934210f7cb7836c8
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-27 12:43:49 +0530

    Time: 120 ms (41.65%), Space: 14.9 MB (45.37%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     //BINARY SEARCH IN A 2D MATRIX
     bool searchMatrix(vector<vector<int>>& matrix,int target){
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int start=0;
-        int end=n-1;
-        while(start>=0 && start<m && end>=0 && end<n){
-            if(matrix[start][end]==target) return true;
-            else if(matrix[start][end]>target) end--;
-            else if(matrix[start][end]<target) start++;
+        int n=matrix.size(),m=matrix[0].size();
+        int i=0,j=m-1;
+        while(i>=0 && i<n && j>=0 && j<m){
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]>target) j--; 
+            else if(matrix[i][j]<target) i++;
         }
         return false;
+        // int m=matrix.size();
+        // int n=matrix[0].size();
+        // int start=0;
+        // int end=n-1;
+        // while(start>=0 && start<m && end>=0 && end<n){
+        //     if(matrix[start][end]==target) return true;
+        //     else if(matrix[start][end]>target) end--;
+        //     else if(matrix[start][end]<target) start++;
+        // }
+        // return false;
     }
 };
