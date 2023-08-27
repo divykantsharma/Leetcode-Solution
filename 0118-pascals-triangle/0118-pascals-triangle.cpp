10f367b7c91b21c88ca7fd9aac015c938c7a994d
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-27 13:45:24 +0530

    Time: 3 ms (40.88%), Space: 6.9 MB (17.55%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    //BRUTE FORCE - TC->O(N*N*R)~O(N^3)
+    int ncr(int n,int r){
+        long long int res=1;
+        for(int i=0;i<r;i++){
+            res=res*(n-i);
+            res=res/(i+1);
+        }
+        return res;
+    }
+    vector<vector<int>> generate(int n){
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            vector<int> temp;
+            for(int j=0;j<=i;j++){
+                int val=ncr(i,j);
+                temp.push_back(val);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
