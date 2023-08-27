86fe5e27f57b5475ae1b379e41576fdd1f7e887a
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-27 11:12:26 +0530

    Time: 3 ms (74.58%), Space: 9.6 MB (28.18%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -6,7 +6,7 @@ public:
         int i=0,j=m-1;
         while(i>=0 && i<n && j>=0 && j<m){
             if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]>target) j--;
+            else if(matrix[i][j]>target) j--; 
             else if(matrix[i][j]<target) i++;
         }
         return false;
