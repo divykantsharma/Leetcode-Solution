0db194afb1ccd318e70440a913d013f8f330736e
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-08-27 13:52:19 +0530

    Time: 7 ms (83.08%), Space: 10 MB (72.42%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -5,7 +5,7 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<int> prev(n,0);
-        for(int i=0;i<m;i++){
+        for(int i=0;i<m;i++){ 
             vector<int> cur(n,0);
             for(int j=0;j<n;j++){
                 if(i==0 && j==0) cur[j]=grid[i][j];
