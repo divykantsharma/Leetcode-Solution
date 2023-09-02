2634a58ae048bfae39bba6f7574da1753cb272cb
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-09-02 10:12:35 +0530

    Time: 91 ms (89.63%), Space: 64.8 MB (76.8%) - LeetHub

---
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -0,0 +1,45 @@
+class Solution{
+public:
+    //MEMOIZATION
+    int f(string& s,unordered_map<string,int>& mp,int index,vector<int>& dp){
+        if(index>=s.length()) return 0;
+        if (dp[index] != -1) return dp[index];
+        string temp="";
+        int minExtra=s.length();
+        for(int i=index;i<s.length();i++){
+            temp.push_back(s[i]);
+            int tempExtra=(mp.count(temp)?0:temp.length());
+            int nextExtra=f(s,mp,i+1,dp);
+            int totalExtra=tempExtra+nextExtra;
+            minExtra=min(minExtra,totalExtra);
+        }
+        return dp[index]=minExtra;
+    }
+    int minExtraChar(string s,vector<string>& dictionary){
+        unordered_map<string,int> mp;
+        for(string it:dictionary) mp[it]++;
+        vector<int> dp(s.length(),-1);
+        return f(s,mp,0,dp);
+    }
+
+
+    //RECURSION
+    // int f(string& s,unordered_map<string,int>& mp,int index){
+    //     if(index>=s.length()) return 0;
+    //     string temp="";
+    //     int minExtra=s.length();
+    //     for(int i=index;i<s.length();i++){
+    //         temp.push_back(s[i]);
+    //         int tempExtra=(mp.count(temp)?0:temp.length());
+    //         int nextExtra=f(s,mp,i+1);
+    //         int totalExtra=tempExtra+nextExtra;
+    //         minExtra=min(minExtra,totalExtra);
+    //     }
+    //     return minExtra;
+    // }
+    // int minExtraChar(string s,vector<string>& dictionary){
+    //     unordered_map<string,int> mp;
+    //     for(string it:dictionary) mp[it]++;
+    //     return f(s,mp,0);
+    // }
+};
