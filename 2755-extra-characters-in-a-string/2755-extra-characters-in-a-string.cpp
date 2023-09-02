17f09250f7c11121ae2dd2535c96333619c53c0d
Divy Kant Sharma <89973756+divykantsharma@users.noreply.github.com>
2023-09-02 10:20:48 +0530

    Time: 99 ms (85.25%), Space: 62.3 MB (91.23%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,25 +1,25 @@
 class Solution{
 public:
     //MEMOIZATION
-    int f(string& s,unordered_map<string,int>& mp,int index,vector<int>& dp){
+    int f(string& s,unordered_set<string>& st,int index,vector<int>& dp){
         if(index>=s.length()) return 0;
         if (dp[index] != -1) return dp[index];
         string temp="";
         int minExtra=s.length();
         for(int i=index;i<s.length();i++){
             temp.push_back(s[i]);
-            int tempExtra=(mp.count(temp)?0:temp.length());
-            int nextExtra=f(s,mp,i+1,dp);
+            int tempExtra=(st.count(temp)?0:temp.length());
+            int nextExtra=f(s,st,i+1,dp);
             int totalExtra=tempExtra+nextExtra;
             minExtra=min(minExtra,totalExtra);
         }
         return dp[index]=minExtra;
     }
     int minExtraChar(string s,vector<string>& dictionary){
-        unordered_map<string,int> mp;
-        for(string it:dictionary) mp[it]++;
+        unordered_set<string> st(dictionary.begin(),dictionary.end());
+        // for(string it:dictionary) mp[it]++;
         vector<int> dp(s.length(),-1);
-        return f(s,mp,0,dp);
+        return f(s,st,0,dp);
     }
 
 
